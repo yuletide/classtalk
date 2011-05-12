a51011e6527af0e4d61b54f57b39c525fa4efe36
T S <YenTheFirst@gmail.com>
2011-05-12 14:20:21 -0700

    added reply_to

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,6 +1,9 @@
 class NotificationMailer < ActionMailer::Base
   def notification_email(message,user,group)
-    mail(:to=>user.email, :from=>"group+#{group.id}@classtalk-staging.heroku.com",:subject=>"Update from #{group.title}") do |format|
+    mail(:to=>user.email, 
+      :from=>"group+#{group.id}@classtalk-staging.heroku.com",
+      :subject=>"Update from #{group.title}",
+      :reply_to => env['CLOUDMAILIN_FORWARD_ADDRESS']) do |format|
       format.text {render :text=>message}
     end
   end
