4e93bd3ced374bb46f7cc53d55113e19b04dbad5
Scott Silverman <scottsil@me.com>
2011-06-03 12:12:05 -0700

    Now redirecting traffic from classtalk.heroku.com to classtalk.org

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :ensure_domain
+  
+  APP_DOMAIN = 'classtalk.org'
   
   def index
     if admin_signed_in?
@@ -10,5 +13,12 @@ class ApplicationController < ActionController::Base
       redirect_to new_user_session_path
     end
     #render login page by default
+  end
+  
+  def ensure_domain
+    if request.env['HTTP_HOST'] != APP_DOMAIN
+      # HTTP 301 is a "permanent" redirect
+      redirect_to "http://#{APP_DOMAIN}", :status => 301
+    end
   end
 end
