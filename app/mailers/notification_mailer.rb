237af663bfcddf7b0b839f7d8ca1b488f2824fc8
Scott Silverman <scottsil@me.com>
2011-06-13 18:10:28 -0700

    change mailer to use heroku key APP_DOMAIN for mail domain

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,7 +1,7 @@
 class NotificationMailer < ActionMailer::Base
   def notification_email(message,user,group)
     mail(:to=>user.email, 
-      :from=>"group+#{group.id}@mail.staging.classtalk.org",
+      :from=>"group+#{group.id}@mail.ENV["APP_DOMAIN"]",
       :subject=>"Update from #{group.title}") do |format|
       format.text {render :text=>message}
     end
