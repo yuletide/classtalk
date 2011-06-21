9273c500a10c86bfc848b4b3651672592d8a8d0f
Scott Silverman <scottsil@me.com>
2011-06-21 15:12:01 -0700

    domain only ensured in production

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  # before_filter :ensure_domain
+  before_filter :ensure_domain if Rails.env.production?
     
   def index
     if admin_signed_in?
@@ -13,10 +13,10 @@ class ApplicationController < ActionController::Base
     #render login page by default
   end
   
-  # def ensure_domain
-  #   if request.env['HTTP_HOST'] != ENV["APP_DOMAIN"]
-  #     # HTTP 301 is a "permanent" redirect
-  #     redirect_to "http://#{ENV["APP_DOMAIN"]}", :status => 301
-  #   end
-  # end
+  def ensure_domain
+    if request.env['HTTP_HOST'] != ENV["APP_DOMAIN"]
+      # HTTP 301 is a "permanent" redirect
+      redirect_to "http://#{ENV["APP_DOMAIN"]}", :status => 301
+    end
+  end
 end
