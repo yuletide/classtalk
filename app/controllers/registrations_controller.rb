2b5cbeb0fda5252520f40872ed017e5136eecad0
T S <YenTheFirst@gmail.com>
2011-06-22 11:29:21 -0700

    set auth_scope to hit all the necessary methods

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,5 +1,5 @@
 class RegistrationsController < Devise::RegistrationsController
-  prepend_before_filter :authenticate_scope!, :only=>:edit_password
+  prepend_before_filter :authenticate_scope!, :only=>[:edit,:update,:destroy,:edit_password]
   def edit_password
   end
 end
