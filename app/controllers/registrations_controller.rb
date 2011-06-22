780bacebebcfd1d70aa6d1fd98dac8b8e0d45c1a
T S <YenTheFirst@gmail.com>
2011-06-22 11:23:41 -0700

    change the auth_scope limit, so that the registration functions are publically accessible again

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,5 +1,5 @@
 class RegistrationsController < Devise::RegistrationsController
-  prepend_before_filter :authenticate_scope!
+  prepend_before_filter :authenticate_scope!, :only=>:edit_password
   def edit_password
   end
 end
