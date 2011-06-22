42b9bcd11cd32843b14452774809834f2ab87a7a
Scott Silverman <scottsil@me.com>
2011-06-21 17:01:31 -0700

    redirect to welcome splash if no user signed in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,14 +3,12 @@ class ApplicationController < ActionController::Base
   before_filter :ensure_domain if Rails.env.production?
     
   def index
-    if admin_signed_in?
-      redirect_to :users
-    elsif user_signed_in?
+    if user_signed_in?
       redirect_to :groups
     else
-      redirect_to new_user_session_path
+      redirect_to "/welcome"
     end
-    #render login page by default
+    #render welcome page by default
   end
   
   def ensure_domain
