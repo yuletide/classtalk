f45426c9d233418a57e9408a92f2c32307cb6b91
T S <YenTheFirst@gmail.com>
2011-06-22 11:29:42 -0700

    change validations to require name, display name, etc. off the bat, not after creation

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,13 +12,7 @@ class User < ActiveRecord::Base
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me, :name, :display_name, :phone_number
   
-  validates_presence_of :name, :if=>:confirmation_sent_at?
-  validates_presence_of :display_name, :if=>:confirmation_sent_at?
+  validates_presence_of :name
+  validates_presence_of :display_name
   validates_phone_number :phone_number, :allow_nil=>true, :allow_blank=>true
-  
-  validates_presence_of :password_confirmation, :if=>:password_required?
-  private
-  def password_required?
-    (confirmation_sent_at? && encrypted_password.blank?) || password.present? || password_confirmation.present?
-  end
 end
