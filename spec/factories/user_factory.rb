c7b339832c4ea6c8a2d6ac3c0bc2ce6a876b48e8
Justin Stoller <justin.stoller@gmail.com>
2011-07-15 15:45:45 -0700

    changed factory for user model

--- spec/factories/user_factory.rb
+++ spec/factories/user_factory.rb
@@ -2,6 +2,7 @@ Factory.define(:user) do |u|
   u.sequence(:email) {|n| "email#{n}@testcompany.com"}
   u.password "123456"
   u.password_confirmation "123456"
-  u.name "Daninator"
+  u.first_name "Dan"
+  u.last_name "inator"
   u.display_name "melt-on"
 end
