30053e142638f5187aa3c3d792f202fd5c88b92d
T S <YenTheFirst@gmail.com>
2011-08-02 14:26:59 -0700

    have destination manage order_index of questions
    basically, instead of trying to work in a jquery thing, we'll just override #questions_attributes=,
    and have it set an order based on submitted order of parameters.

--- app/models/destination.rb
+++ app/models/destination.rb
@@ -5,6 +5,33 @@ class Destination < ActiveRecord::Base
   has_many :checkins
   
   accepts_nested_attributes_for :questions, :allow_destroy => true, :reject_if => :all_blank
+  def questions_attributes_with_reordering=(attributes_collection)
+    #this portion makes sure we have an array of attributes. it's taken from the original assign_nested_attributes_for_collection_association source
+    #don't know how to DRY it. ah well.
+    if attributes_collection.is_a? Hash
+      keys = attributes_collection.keys
+      attributes_collection = if keys.include?('id') || keys.include?(:id)
+        Array.wrap(attributes_collection)
+      else
+        #but this bit is changed.
+        attributes_collection.sort_by { |i, _| i.to_s.gsub(/\D/,'').to_i }.map { |_, attributes| attributes }
+      end
+    end
+
+    #WARNING: this may fail in weird ways if you only do 'partial updates'
+    #TODO: make that safer.
+    attributes_collection = attributes_collection.each_with_index.map do |att,i|
+      att.merge(:order_index => i)
+    end
+    puts "calling with: #{attributes_collection.inspect}"
+    
+    #for some weird reason, 
+    #  questions_attributes_without_reordering=(attributes_collection)
+    #doesn't work. for now, I'll just assign directly. TODO: investigate more.
+    assign_nested_attributes_for_collection_association(:questions,attributes_collection)
+  end
+
+  alias_method_chain :questions_attributes=, :reordering
 
   def checkin(student)
     return nil unless self.group == student.group
