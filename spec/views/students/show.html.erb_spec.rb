fd1be1d3a2d48016b959cce993bc93c81a757d41
T S <YenTheFirst@gmail.com>
2011-08-02 21:22:44 -0700

    updated student show spec to add @grouped_answers

--- spec/views/students/show.html.erb_spec.rb
+++ spec/views/students/show.html.erb_spec.rb
@@ -4,6 +4,7 @@ describe "students/show.html.erb" do
   before(:each) do
     @student = assign(:student, stub_model(Student))
     @group = assign(:group, Factory.create(:group))
+    assign(:grouped_answers, {Factory.create(:destination) => [stub_model(Answer)]})
   end
 
   it "renders attributes in <p>" do
