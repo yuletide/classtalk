35cd0eab32443bff9be689bab270327daefe2c28
Scott Silverman <scottsil@me.com>
2011-08-18 13:00:18 -0400

    style group nav and member list

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -37,16 +37,46 @@ $(document).ready(function() {
 			}
 		});
 	});
-	$('#message_nav').hover(over, out);
+	$('#message_nav').hover(overMessages, outMessages);
 		//Tell the browser to change the background when hovered over
 		var messagesGroupNav = document.getElementById("messages");
-		function over(event) {
+		function overMessages(event) {
 			messagesGroupNav.style.display = "block";
 		}
 		//tell the browser to change the background to nothing when going outside the object area
-		function out(event) {
+		function outMessages(event) {
 			messagesGroupNav.style.display = "none";
+	}
+	$('#checkins_nav').hover(overCheckins, outCheckins);
+		//Tell the browser to change the background when hovered over
+		var checkinsGroupNav = document.getElementById("checkins");
+		function overCheckins(event) {
+			checkinsGroupNav.style.display = "block";
 		}
+		//tell the browser to change the background to nothing when going outside the object area
+		function outCheckins(event) {
+			checkinsGroupNav.style.display = "none";
+	}
+	$('#members_nav').hover(overMembers, outMembers);
+		//Tell the browser to change the background when hovered over
+		var membersGroupNav = document.getElementById("members");
+		function overMembers(event) {
+			membersGroupNav.style.display = "block";
+		}
+		//tell the browser to change the background to nothing when going outside the object area
+		function outMembers(event) {
+			membersGroupNav.style.display = "none";
+	}
+	$('#settings_nav').hover(overSettings, outSettings);
+		//Tell the browser to change the background when hovered over
+		var settingsGroupNav = document.getElementById("settings");
+		function overSettings(event) {
+			settingsGroupNav.style.display = "block";
+		}
+		//tell the browser to change the background to nothing when going outside the object area
+		function outSettings(event) {
+			settingsGroupNav.style.display = "none";
+	}
 });
 //On click of Message link, hides Members and displays send message
 function toggleMessage() {
