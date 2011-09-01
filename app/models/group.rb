0a48860d18fbb1bccb0bddf6e19f83b24e53da47
T S <YenTheFirst@gmail.com>
2011-09-01 14:57:37 -0700

    trap exceptions in send_message. it can't fail.

--- app/models/group.rb
+++ app/models/group.rb
@@ -10,21 +10,26 @@ class Group < ActiveRecord::Base
   validates_phone_number :phone_number
   
   def send_message(message,sending_person,recipients=nil)
-    recipients ||= students+[user] - [sending_person]
-    
-    #send sms messages to sms recipients
-    numbers = recipients.map(&:phone_number).compact
-    $outbound_flocky.message phone_number, message, numbers
-    
-    #log sms poutputs
-    numbers.each do |destination|
-      LoggedMessage.create(:group=>self,:sender=>sending_person,:source_phone=>phone_number,:destination_phone=>destination,:message=>message)
+    begin
+      recipients ||= students+[user] - [sending_person]
+      
+      #send sms messages to sms recipients
+      numbers = recipients.map(&:phone_number).compact
+      $outbound_flocky.message phone_number, message, numbers
+      
+      #log sms poutputs
+      numbers.each do |destination|
+        LoggedMessage.create(:group=>self,:sender=>sending_person,:source_phone=>phone_number,:destination_phone=>destination,:message=>message)
+      end
+  
+      #send message to non-sms recipients (email, for now)
+      recipients.select {|r| r.email.present?}.each {|recip| NotificationMailer.notification_email(message,recip,self).deliver}
+      
+      #todo: also log these messages
+    rescue Exception=>e
+      logger.error e.message
+      logger.error e.backtrace
     end
-
-    #send message to non-sms recipients (email, for now)
-    recipients.select {|r| r.email.present?}.each {|recip| NotificationMailer.notification_email(message,recip,self).deliver}
-    
-    #todo: also log these messages
   end
   handle_asynchronously :send_message
 
