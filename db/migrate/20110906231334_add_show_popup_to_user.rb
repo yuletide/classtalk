5ad9cb082fa838574776a4b75506645d2c3f7b61
Scott Silverman <scottsil@me.com>
2011-09-07 14:10:08 -0700

    set default value of show_group_number_popup to true

--- db/migrate/20110906231334_add_show_popup_to_user.rb
+++ db/migrate/20110906231334_add_show_popup_to_user.rb
@@ -1,6 +1,9 @@
 class AddShowPopupToUser < ActiveRecord::Migration
   def self.up
     add_column :users, :show_group_number_popup, :boolean, :default => true
+    User.find_each do |user|
+      user.update_attribute(:show_group_number_popup, true)
+    end
   end
 
   def self.down
