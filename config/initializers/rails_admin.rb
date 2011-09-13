ed313a387391d1c2fbeb7df2cdf007e6a5bf6c9e
Erik Michaels-Ober <sferik@gmail.com>
2011-09-13 09:59:49 -0700

    Replace deprecated RailsAdmin.authenticate_with with RailsAdmin.config

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -1,3 +1,5 @@
-RailsAdmin.authenticate_with do
-  redirect_to new_admin_session_path unless current_admin && current_admin.email == "admin@codeforamerica.org"
+RailsAdmin.config do |config|
+  config.authenticate_with do
+    redirect_to new_admin_session_path unless current_admin && current_admin.email == "admin@codeforamerica.org"
+  end
 end
