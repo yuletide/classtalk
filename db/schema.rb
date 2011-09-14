2030c633e9c9fddfe1b4be28ddbd96f476623345
Erik Michaels-Ober <sferik@gmail.com>
2011-09-14 02:08:09 -0700

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -14,18 +14,18 @@
 ActiveRecord::Schema.define(:version => 20110906231334) do
 
   create_table "admins", :force => true do |t|
-    t.string    "email",                             :default => "", :null => false
-    t.string    "encrypted_password", :limit => 128, :default => "", :null => false
-    t.integer   "sign_in_count",                     :default => 0
-    t.timestamp "current_sign_in_at"
-    t.timestamp "last_sign_in_at"
-    t.string    "current_sign_in_ip"
-    t.string    "last_sign_in_ip"
-    t.integer   "failed_attempts",                   :default => 0
-    t.string    "unlock_token"
-    t.timestamp "locked_at"
-    t.timestamp "created_at"
-    t.timestamp "updated_at"
+    t.string   "email",                             :default => "", :null => false
+    t.string   "encrypted_password", :limit => 128, :default => "", :null => false
+    t.integer  "sign_in_count",                     :default => 0
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.integer  "failed_attempts",                   :default => 0
+    t.string   "unlock_token"
+    t.datetime "locked_at"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   add_index "admins", ["email"], :name => "index_admins_on_email", :unique => true
@@ -46,16 +46,16 @@ ActiveRecord::Schema.define(:version => 20110906231334) do
   end
 
   create_table "delayed_jobs", :force => true do |t|
-    t.integer   "priority",   :default => 0
-    t.integer   "attempts",   :default => 0
-    t.text      "handler"
-    t.text      "last_error"
-    t.timestamp "run_at"
-    t.timestamp "locked_at"
-    t.timestamp "failed_at"
-    t.string    "locked_by"
-    t.timestamp "created_at"
-    t.timestamp "updated_at"
+    t.integer  "priority",   :default => 0
+    t.integer  "attempts",   :default => 0
+    t.text     "handler"
+    t.text     "last_error"
+    t.datetime "run_at"
+    t.datetime "locked_at"
+    t.datetime "failed_at"
+    t.string   "locked_by"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   add_index "delayed_jobs", ["priority", "run_at"], :name => "delayed_jobs_priority"
@@ -69,24 +69,24 @@ ActiveRecord::Schema.define(:version => 20110906231334) do
   end
 
   create_table "groups", :force => true do |t|
-    t.integer   "user_id"
-    t.string    "title"
-    t.text      "description"
-    t.timestamp "created_at"
-    t.timestamp "updated_at"
-    t.string    "phone_number"
-    t.string    "destination_phone_number"
+    t.integer  "user_id"
+    t.string   "title"
+    t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "phone_number"
+    t.string   "destination_phone_number"
   end
 
   create_table "logged_messages", :force => true do |t|
-    t.integer   "group_id"
-    t.integer   "sender_id"
-    t.string    "sender_type"
-    t.string    "source_phone"
-    t.string    "destination_phone"
-    t.string    "message"
-    t.timestamp "created_at"
-    t.timestamp "updated_at"
+    t.integer  "group_id"
+    t.integer  "sender_id"
+    t.string   "sender_type"
+    t.string   "source_phone"
+    t.string   "destination_phone"
+    t.string   "message"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "questions", :force => true do |t|
@@ -98,48 +98,48 @@ ActiveRecord::Schema.define(:version => 20110906231334) do
   end
 
   create_table "rails_admin_histories", :force => true do |t|
-    t.string    "message"
-    t.string    "username"
-    t.integer   "item"
-    t.string    "table"
-    t.integer   "month"
-    t.integer   "year"
-    t.timestamp "created_at"
-    t.timestamp "updated_at"
+    t.string   "message"
+    t.string   "username"
+    t.integer  "item"
+    t.string   "table"
+    t.integer  "month",      :limit => 2
+    t.integer  "year",       :limit => 5
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   add_index "rails_admin_histories", ["item", "table", "month", "year"], :name => "index_histories_on_item_and_table_and_month_and_year"
 
   create_table "students", :force => true do |t|
-    t.string    "name"
-    t.string    "phone_number"
-    t.timestamp "created_at"
-    t.timestamp "updated_at"
-    t.integer   "group_id"
-    t.string    "email"
-    t.integer   "active_checkin_id"
+    t.string   "name"
+    t.string   "phone_number"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "group_id"
+    t.string   "email"
+    t.integer  "active_checkin_id"
   end
 
   create_table "users", :force => true do |t|
-    t.string    "email",                                  :default => "",   :null => false
-    t.string    "encrypted_password",      :limit => 128, :default => "",   :null => false
-    t.string    "reset_password_token"
-    t.timestamp "remember_created_at"
-    t.integer   "sign_in_count",                          :default => 0
-    t.timestamp "current_sign_in_at"
-    t.timestamp "last_sign_in_at"
-    t.string    "current_sign_in_ip"
-    t.string    "last_sign_in_ip"
-    t.timestamp "created_at"
-    t.timestamp "updated_at"
-    t.string    "phone_number"
-    t.string    "display_name"
-    t.string    "confirmation_token"
-    t.timestamp "confirmed_at"
-    t.timestamp "confirmation_sent_at"
-    t.string    "first_name"
-    t.string    "last_name"
-    t.boolean   "show_group_number_popup",                :default => true
+    t.string   "email",                                  :default => "",   :null => false
+    t.string   "encrypted_password",      :limit => 128, :default => "",   :null => false
+    t.string   "reset_password_token"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",                          :default => 0
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "phone_number"
+    t.string   "display_name"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.boolean  "show_group_number_popup",                :default => true
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
