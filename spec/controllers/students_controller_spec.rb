c1ef5a98a02f4f05e430b3be2263ba129c11455a
T S <YenTheFirst@gmail.com>
2011-09-13 17:26:31 -0700

    update studentcontroller specs to match how student controller should work

--- spec/controllers/students_controller_spec.rb
+++ spec/controllers/students_controller_spec.rb
@@ -1,6 +1,10 @@
 require 'spec_helper'
 
 describe StudentsController do
+  before(:each) do
+    @current_user = login
+  end
+  
   describe "authorization" do
     pending "all actions should be accessible to logged in users" do
     end
@@ -22,10 +26,9 @@ describe StudentsController do
       response.should redirect_to(members_group_path(@group))
     end
     
-    it "on failure, should redirect to members_group_page, with errors" do
+    it "on failure, should render the same page, with errors" do
       @group = Factory.create(:group)
       post :create_multiple, :group_id => @group.id, :group => {"students_attributes" => {"placeholder_name" => {"name"=>"TestName1", "phone_number"=>"555", "email"=>"testname1@example.com", "_destroy"=>"false"}, "placeholder_name2" => {"name"=>"TestName2", "phone_number"=>"6665554444", "email"=>"testname2@example.com", "_destroy"=>"false"} } }
-      response.should redirect_to(members_group_path(@group))
       @group.reload.students.count.should == 0
       assigns[:group].errors.should_not be_empty
     end
